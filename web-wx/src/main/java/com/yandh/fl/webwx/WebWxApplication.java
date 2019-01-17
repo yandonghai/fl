b6742879ea9928d5804f81d3571ad0be5753649d
yandh <445288363@qq.com>
2019-01-17 18:54:25 +0800

    项目初始化提交

---
+++ web-wx/src/main/java/com/yandh/fl/webwx/WebWxApplication.java
@@ -0,0 +1,14 @@
+package com.yandh.fl.webwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebWxApplication.class, args);
+    }
+
+}
+
---
+++ web-wx/src/main/java/com/yandh/fl/webwx/controller/GoodsController.java
@@ -0,0 +1,9 @@
+package com.yandh.fl.webwx.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GoodsController {
+
+
+}
